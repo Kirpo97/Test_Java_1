d2c27501dfcf3ea5f20b8f87a4fbe9628cf3cd7e
Kirill <k007pm@yandex.ru>
2022-01-24 22:27:52 +0500

    v-4

--- TestJava1.java
+++ TestJava1.java
@@ -2,5 +2,6 @@ public class TestJava1{
     public static void main(String[] args) {
         System.out.println("Hello Java 1");
         System.out.println("Hello Java 2");
+        System.out.println("Hello Java 3");
     }
 }
